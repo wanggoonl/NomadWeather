f626aa9aed239187dcbcc1effbc81a71081b1ea7
WanggoonL <87754634+WanggoonL@users.noreply.github.com>
2021-12-29 13:25:09 +0900

    Done make this weather app

--- App.js
+++ App.js
@@ -8,9 +8,21 @@ import {
   ScrollView,
   ActivityIndicator,
 } from "react-native";
+import { Fontisto } from "@expo/vector-icons";
+
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 const API_KEY = "8b09092551c9ffa1cf54d5e4169e4a98";
+
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
 
 export default function App() {
   const [city, setCity] = useState("Loading...");
@@ -56,13 +68,17 @@ export default function App() {
         ) : (
           days.map((day, index) => (
             <View key={index} style={styles.day}>
-              <Text style={styles.temp}>
-                {parseFloat(day.temp.day).toFixed(1)}
-              </Text>
+              <View style={styles.weather}>
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={50}
+                ></Fontisto>
+              </View>
               <Text style={styles.description}>{day.weather[0].main}</Text>
-              <Text style={styles.description}>
-                {day.weather[0].description}
-              </Text>
+              <Text style={styles.tiny}>{day.weather[0].description}</Text>
             </View>
           ))
         )}
@@ -85,18 +101,26 @@ const styles = StyleSheet.create({
     fontSize: 58,
     fontWeight: "500",
   },
-  weather: {},
+  weather: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "80%",
+    marginTop: -10,
+  },
   day: {
     width: SCREEN_WIDTH,
     alignItems: "center",
   },
   temp: {
     marginTop: 50,
-    fontWeight: "600",
-    fontSize: 178,
+    fontWeight: "500",
+    fontSize: 88,
   },
   description: {
-    marginTop: -30,
-    fontSize: 60,
+    fontSize: 30,
+  },
+  tiny: {
+    fontSize: 20,
   },
 });
